b5409d3882aff8cb3efdcff86e183237cb3bcafe
MyBlogAuthor <blog@example.com>
2026-05-23 22:38:25 +0800

    feat: scaffold project with design tokens and tab bar config

---
+++ miniprogram/app.js
@@ -0,0 +1,18 @@
+// miniprogram/app.js
+App({
+  onLaunch: function () {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+    } else {
+      wx.cloud.init({
+        env: 'YOUR_ENV_ID',
+        traceUser: true,
+      });
+    }
+  },
+
+  globalData: {
+    userInfo: null,
+    openid: null,
+  },
+});
